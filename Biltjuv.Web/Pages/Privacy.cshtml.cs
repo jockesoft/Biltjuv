468875eb487064175eb42934557b206163412b97
Joakim Fredlund <joakim@fredlund.me>
2026-09-13 15:56:59 +0400

    first commit

---
+++ Biltjuv.Web/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Biltjuv.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
